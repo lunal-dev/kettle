a8e7766744c6d5b968f181c1a226e9b68525e108
André Arko <andre@arko.net>
2026-02-25 15:02:05 -0800

    start hooking up attestation

--- src/commands/attest.rs
+++ src/commands/attest.rs
@@ -3,14 +3,21 @@ use std::path::PathBuf;
 
 #[cfg(feature = "attest")]
 pub(crate) async fn attest(path: &PathBuf) -> Result<()> {
-    let platform = attestation::detect().expect("no TEE platform detected");
-    eprintln!("Detected platform: {}", platform);
+    crate::commands::build::build(&path)?;
 
-    let nonce = b"hello-attestation";
-    let evidence_json = attestation::attest(platform, nonce)
+    let platform = attestation::detect().expect("no TEE platform detected");
+    eprintln!("Attesting build on platform: {}", platform);
+
+    let provenance_bytes = fs_err::read("provenance.json");
+    let provenance_value = serde_json::from_slice(provenance_bytes);
+    let provenance_checksum_bytes = serde_json::to_string(provenance_value);
+    let provenance_checksum = sha2::Sha256::digest(provenance_checksum_bytes);
+
+    let evidence_json = attestation::attest(platform, provenance_checksum)
         .await
         .expect("attestation failed");
-    println!("{}", String::from_utf8_lossy(&evidence_json));
+    fs_err::write("evidence.json", String::from_utf8_lossy(&evidence_json));
+    println!("Attestation complete! Evidence written to file `evidence.json`");
 
     Ok(())
 }
