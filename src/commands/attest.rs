0026b5db01ade6bdde317408288d3e3515518bd4
André Arko <andre@arko.net>
2026-02-26 16:57:43 -0800

    print the provenance checksum we will attest

--- src/commands/attest.rs
+++ src/commands/attest.rs
@@ -9,12 +9,16 @@ pub(crate) async fn attest(path: &PathBuf) -> Result<()> {
     crate::commands::build::build(path)?;
 
     let platform = attestation::detect().expect("no TEE platform detected");
-    eprintln!("Attesting build on platform: {}", platform);
+    println!("Running on platform: {}", platform);
 
     let provenance_bytes = fs_err::read(path.join("kettle-build/provenance.json"))?;
     let provenance_value: serde_json::Value = serde_json::from_slice(&provenance_bytes)?;
     let provenance_checksum_bytes = serde_json::to_string(&provenance_value)?;
     let provenance_checksum = sha2::Sha256::digest(provenance_checksum_bytes);
+    println!(
+        "Attesting build provenance.json with checksum {}",
+        hex::encode(provenance_checksum)
+    );
 
     let evidence_json = attestation::attest(platform, provenance_checksum.as_slice())
         .await
